029c37a4a6109fb06c24765e92c41522c1d15e6b
wxkvito <reedevild@gmail.com>
2020-03-16 13:39:31 +0400

    lab2 updated

---
+++ lab2/src/app/table/table.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit {
+  masive = []
+
+
+  constructor() { }
+
+  ngOnInit() {
+    this.masive = [
+      {name:'dato',
+      lastname:'seiranovi',
+      status:'student',
+      id:'25001892043'
+    },
+     {name:'vaxo',
+     lastname:'ozgebishvili',
+     status:'Student',
+     id:'25561843043'
+  },
+    {name:'lasha',
+    lastname:'tsevelidze',
+    status:'Student',
+    id:'25001892043'
+}
+
+]
+    
+  }
+  
+
+}
